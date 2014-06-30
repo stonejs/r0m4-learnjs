033fb7fdcaff3e375a99822878a6edd3e44d38f7
r0m4 <formicrotech@gmail.com>
2014-06-30 18:57:30 +0400

    готов перебор массива для двух элементов!

--- rosettaCode/abs.js
+++ rosettaCode/abs.js
@@ -1,50 +1,558 @@
-/*
+/*А тебе нужно вывести для того ввода ([[1,2],[3,4],[5,6],[7,8]]) восемь
+строк, в каждой по три индекса.
+Давай я тебе их руками покажу:
 
-["a","bb","dd"]
-["a","bb","e"]
-["a","cc","dd"]
-["a","cc","e"]
-["b","bb","dd"]
-["b","bb","e"]
-["b","cc","dd"]
-["b","cc","e"]
-["c","bb","dd"]
-["c","bb","e"]
-["c","cc","dd"]
-["c","cc","e"]
+[[1,3,5,7],
+ [1,3,5,8],
+ [1,3,6,7],
+ [1,3,6,8],
+ [1,4,5,7],
+ [1,4,5,8],
+ [1,4,6,7],
+ [1,4,6,8],
+ [2,3,5,7],
+ [2,3,5,8],
+ [2,3,6,7],
+ [2,3,6,8],
+ [2,4,5,7],
+ [2,4,5,8],
+ [2,4,6,7],
+ [2,4,6,8]]
 
-Еще раз, "программа выводит ВСЕ возможные РАЗЛИЧНЫЕ массивы длины N,
-где на i-м месте -- любая строка из i-го массива во входящем массиве".
-Где у тебя перебор всех вариантов?
 
-Еще раз, для ввода [["a","b","c"], ["bb","cc"], ["dd","e"]]. Имеем
-массив из трех массивов. Значит, нужно выводить массив из трех
-строчек. На первом месте может быть любая из строчек первого массива
-(т.е. одна из "a","b","c"), на втором -- любая из второго массива
-(т.е. одна из "bb","cc"), на третьем -- любая из "dd","e". Например,
-берем "a" из первого, "cc" из второго, "d" из третьего, т.е. ["a",
-"cc", "d"]. Перебор всех вариантов дает вывод, приведенный сверху.
+[[0,0,0,0],    
+ [0,0,0,1],
+ [0,0,1,0],
+ [0,0,1,1],
+ [0,1,0,0],
+ [0,1,0,1],
+ [0,1,1,0],
+ [0,1,1,1],
+ [1,0,0,0],
+ [1,0,0,1],
+ [1,0,1,0],
+ [1,0,1,1],
+ [1,1,0,0],
+ [1,1,0,1],
+ [1,1,1,0],
+ [1,1,1,1]]
+ */
 
-Порядок вывода вариантов не важен. В выводе не должно быть двух
-одинаковых массивов, т.е. ["a", "cc", "d"] два раза повторяться не
-может. При этом во входе гарантируется, что в каждом из массивов со
-строчками все строчки разные, т.е. во входном массиве не может быть
-такого массива как ["a", "a", "b"].
-
-Пожалуйста, внимательно прочитай условие еще раз, и подумай, что нужно
-сделать. Задай вопрос, если что-то непонятно. Я, правда, не могу
-представить такого прочтения условия, которое подразумевало бы такое
-решение как ты предложил. */
-
-var bukvi = [["1","2"], ["3","4","9"], ["5","6"], ["7","8"]];
+var bukvi = [["1","2"], ["3","4","5"], ["6","7"], ["8","9"]];
 //var bukvi = [["a","b","c"], ["bb","cc"], ["dd","e"]];
+var cifri0 = [[1,2]];
+
+var cifridva = [[1,2], [3,4]];
+
+var cifri = [[1,2], [3,4], [5,6],[7,8]];
+
+var cifri2 = [[1,2], [3,4], [5,6], [7,8],[9,10]];
+
+
+function variate(arr) {
+    if (arr.length === 0) return "Incorrect Input";
+    if (arr.length === 1) return [[arr[0][0]],[arr[0][1]]]; //вот он !!! Базис рекурсивной функции :):):):)
+    
+
+        var output = [];
+        var m = (arr.slice(0, arr.length));
+        
+
+        for (var i = 0; i < m.length; i++){
+          
+
+            for (var k = 0; k < m[i].length; k++) {
+                var t = [arr[0][0]].concat(m[i][k]);
+                output.push(t);
+
+            };
+
+        };
+
+        for (var i = 0; i < m.length; i++) {
+            
+
+            for (var j = 0; j < m[i].length; j++) {
+                var k = [arr[0][1]].concat(m[i][j]);
+                output.push(k);
+            
+            };
+
+        };  
+
+        variate(arr.slice(0, arr.length-1));
+        
+        return   output;
+
+       
+
+};
+console.log(variate(cifri2));
+
+
+
+function idx(n){
+    if (n===0)return "Incorrect Input";
+    if (n===1)return[[0],[1]];
+
+    //idx(n-1)+idx(n-2);
+
+    var output = [];
+    var m = idx(n-1);
+    
+
+    for (var i = 0; i < m.length; i++) {
+        var t = [0].concat(m[i]);
+        output.push(t);    
+    };
+
+    for (var i = 0; i < m.length; i++) {
+        var k = [1].concat(m[i]);
+        output.push(k);
+    }
+
+    return   output;
+
+    
+
+}
+
+//console.log(idx(5));
+
+
+function idxAdvan(n) {
+      var output = [];
+    var m = idx(n);
+    
+
+    for (var i = 0; i < m.length; i++) {
+        var t = [0].concat(m[i]);
+        output.push(t);    
+    };
+
+    for (var i = 0; i < m.length; i++) {
+        var k = [1].concat(m[i]);
+        output.push(k);
+    }
+
+    return   output;
+};
+
+//console.log(idxAdvan(2));
+
+
+
+function idx1(arr) {
+    return [[0],[1]];
+};
+
+
+
+function idx2(arr) {
+    var output = [];
+    var m = idx1();
+    
+
+    for (var i = 0; i < m.length; i++) {
+        var t = [0].concat(m[i]);
+        output.push(t);    
+    };
+
+    for (var i = 0; i < m.length; i++) {
+        var k = [1].concat(m[i]);
+        output.push(k);
+    }
+
+    return   output;
+};
+
+
+function idx3(arr) {
+    var output = [];
+    var m = idx2();
+
+    for (var i = 0; i < m.length; i++) {
+        var t = [0].concat(m[i]);
+        output.push(t);
+    };
+
+    for (var i = 0; i < m.length; i++) {
+        var k = [1].concat((m[i]));
+        output.push(k);
+    }
+
+    return output;
+}
+
+
+//console.log(idx3(cifri));
+
+function idx4(arr) {
+    var output = [];
+    var m = idx3();
+
+    for (var i = 0; i < m.length; i++) {
+        var t = [0].concat(m[i]);
+        output.push(t);
+    };
+
+    for (var i = 0; i < m.length; i++) {
+        var k = [1].concat(m[i]);
+        output.push(k);
+    }
+    return output;
+}
+
+//console.log(idx4(cifri));
+
+
+function idxN(n) {
+    if (n===1) return arr[1];
+    if (n===0) return arr[0];
+
+    //idxN(arr.slice(0, length-2).concat(idxN(arr.slice(0, length-3))));
+
+    var output = [];
+    
+    
+
+    var m = idx4();
+    
+
+    for (var i = 0; i < m.length; i++) {
+        var t = [0].concat(m[i]);
+        output.push(t);
+    };
+
+    for (var i = 0; i < m.length; i++) {
+        var k = [1].concat(m[i]);
+        output.push(k);
+    }
+    
+    return output;
+}
+
+//console.log(idxN(cifri));
+
+
+
+
+function numeric(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        console.log(i);
+    }
+}
+
+//numeric(cifri);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function nomerArr (arr) {
+    
+    var matrix = [];
+
+    for(var i=0; i<arr.length; i++) {
+        matrix[i] = [];
+
+        for(var j=0; j<arr[i].length; j++) {
+            
+            matrix[i][j]=arr[i][j];
+            
+        }
+    };
+
+    
+
+
+    
+    return matrix;
+}
+
+//console.log(nomerArr(cifri));
+
+
+
+
+
+
+
+
+function vivod (arr) {
+    var output = [];
+    var array = [];
+    for ( var i = 0; i < arr.length; i++) {
+        
+        array.push(arr[i][0]);
+    }
+
+    output.push(array);
+    array = [];
+
+    for (var i = 0; i < arr.length; i++) {
+        if (i != arr.length -1 ) {
+            array.push(arr[i][0]);
+            
+
+        } 
+            if (i===arr.length-1){ 
+            array.push(arr[i][1]);
+            
+
+            }
+
+    };
+
+    output.push(array);
+    array = [];
+
+    for (var i = 0; i < arr.length; i++) {
+        if (i != arr.length -2 ) {
+            array.push(arr[i][0]);
+            
+
+        } 
+            if (i===arr.length-2){ 
+            array.push(arr[i][1]);
+            
+
+            }
+
+    };
+
+    output.push(array);
+    array = [];
+
+    for (var i = 0; i < arr.length; i++) {
+        if (i != arr.length -2 && i != arr.length -1) {
+            array.push(arr[i][0]);
+            
+
+        } 
+            if (i === arr.length -2 || i === arr.length -1){ 
+            array.push(arr[i][1]);
+            
+
+            }
+
+    };
+
+    output.push(array);
+    array = [];
+
+
+    for (var i = 0; i < arr.length; i++) {
+        if (i != arr.length -3 ) {
+            array.push(arr[i][0]);
+            
+
+        } 
+            if (i === arr.length -3){ 
+            array.push(arr[i][1]);
+            
+
+            }
+
+    };
+
+    output.push(array);
+    array = [];
+
+    for (var i = 0; i < arr.length; i++) {
+        if (i != arr.length -3 && i != arr.length -1 ) {
+            array.push(arr[i][0]);
+            
+
+        } 
+            if (i === arr.length -3 || i === arr.length - 1){ 
+            array.push(arr[i][1]);
+            
+
+            }
+
+    };
+
+    output.push(array);
+    array = [];
+ 
+
+    for (var i = 0; i < arr.length; i++) {
+        if (i != arr.length -3 && i != arr.length -2 ) {
+            array.push(arr[i][0]);
+            
+
+        } 
+            if (i === arr.length -3 || i === arr.length - 2){ 
+            array.push(arr[i][1]);
+            
+
+            }
+
+    };
+
+    output.push(array);
+    array = [];
+
+
+    for (var i = 0; i < arr.length; i++) {
+        if (i != arr.length -3 && i != arr.length -1 ) {
+            array.push(arr[i][0]);
+            
+
+        } 
+            if (i === arr.length -3 || i === arr.length - 1){ 
+            array.push(arr[i][1]);
+            
+
+            }
+
+    };
+
+    output.push(array);
+    array = [];
+ 
+
+
+    return output;
+}
+
+//console.log(vivod(cifri));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function simple(arr) {
+    var output = [];
+
+
+
+var c = Array;
+
+for( var i = 0, a = c(9); i < 9; a[i] = c(9), i++ );
+
+//console.log( a.join(",") );
+
+};
+
+//simple();
+
+
+
+function recursion(arr) {
+    var output = [];
+    var semiArr = [];
+
+    semiArr.push(arr[0][0],arr[1][0],arr[2][0],arr[3][0]);
+    output.push(semiArr);
+    semiArr = [];
+
+    semiArr.push(arr[0][0],arr[1][0],arr[2][0],arr[3][1]);
+    output.push(semiArr);
+    semiArr = [];
+
+    semiArr.push(arr[0][0],arr[1][0],arr[2][1],arr[3][0]);
+    output.push(semiArr);
+    semiArr = [];
+
+    semiArr.push(arr[0][0],arr[1][0],arr[2][1],arr[3][1]);
+    output.push(semiArr);
+    semiArr = [];
+
+    semiArr.push(arr[0][0],arr[1][1],arr[2][0],arr[3][0]);
+    output.push(semiArr);
+    semiArr = [];
+
+    semiArr.push(arr[0][0],arr[1][1],arr[2][0],arr[3][1]);
+    output.push(semiArr);
+    semiArr = [];
+
+
+
+    //console.log(output);
+};
+
+//(recursion(bukvi));
+
+
+
+
+
+
+
+
+function arrSorter (arr) {
+    var output = [];
+    semiArr = [];
+
+    var matrix = [];
+
+    for(var i=0; i<arr.length; i++) {
+        matrix[i] = [];
+
+        for(var j=0; j<arr[i].length; j++) {
+            matrix[i].push(arr[i]);
+            
+        }
+    }
+
+   
+    //console.log(matrix);
+
+};   
+
+
+//(arrSorter(bukvi));
+
+
+
+
+
+
+
+
+
 
 function replacer(arr) {
     var newArr = [];
     var novoArr = [];
     var count = 0; 
 
-    for (var i = 0; i < arr[i].length; i++) {
+    for (var i = 0; i < arr.length; i++) {
         for (var k = 0; k < arr.length; k++) {
             newArr.push(arr[k][i]);
             if (arr[k]>count) count++;
@@ -62,6 +570,5 @@ function replacer(arr) {
             };
 };
 
-console.log (replacer(bukvi));
-
+//console.log(replacer(bukvi));
 
